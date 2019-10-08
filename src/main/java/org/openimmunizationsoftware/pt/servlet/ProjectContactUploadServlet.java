9eb38f8faec3f8547a2c62047f624b65dc982dd6
Nathan Bunker <Nathan.Bunker@gmail.com>
2019-10-08 02:35:47 -0600

    PT-1 Started to create upload servlet

---
+++ src/main/java/org/openimmunizationsoftware/pt/servlet/ProjectContactUploadServlet.java
@@ -0,0 +1,112 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.openimmunizationsoftware.pt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.hibernate.Session;
+import org.openimmunizationsoftware.pt.model.WebUser;
+
+/**
+ * 
+ * @author nathan
+ */
+public class ProjectContactUploadServlet extends ClientServlet {
+
+  /**
+   * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+   * methods.
+   * 
+   * @param request
+   *          servlet request
+   * @param response
+   *          servlet response
+   * @throws ServletException
+   *           if a servlet-specific error occurs
+   * @throws IOException
+   *           if an I/O error occurs
+   */
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+    HttpSession session = request.getSession(true);
+    WebUser webUser = (WebUser) session.getAttribute(SESSION_VAR_WEB_USER);
+    if (webUser == null) {
+      RequestDispatcher dispatcher = request.getRequestDispatcher("HomeServlet");
+      dispatcher.forward(request, response);
+      return;
+    }
+
+    PrintWriter out = response.getWriter();
+    try {
+      
+      boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+      if (isMultipart)
+      {
+        
+      }
+      
+
+      printHtmlHead(out, "Contacts", request);
+
+      Session dataSession = getDataSession(session);
+      
+      out.println("<form action=\"ProjectContactUploadServlet\" method=\"POST\" enctype=\"multipart/form-data\"");
+      out.println("  <input type=\"file\" name=\"file\" size=\"50\"/>");
+      out.println("  <input type=\"submit\" name=\"submit\" value=\"Upload Contacts\"");
+      out.println("</form>");
+      printHtmlFoot(out);
+
+    } finally {
+      out.close();
+    }
+  }
+
+  // <editor-fold defaultstate="collapsed"
+  // desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
+  /**
+   * Handles the HTTP <code>GET</code> method.
+   * 
+   * @param request
+   *          servlet request
+   * @param response
+   *          servlet response
+   * @throws ServletException
+   *           if a servlet-specific error occurs
+   * @throws IOException
+   *           if an I/O error occurs
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    processRequest(request, response);
+  }
+
+  /**
+   * Handles the HTTP <code>POST</code> method.
+   * 
+   * @param request
+   *          servlet request
+   * @param response
+   *          servlet response
+   * @throws ServletException
+   *           if a servlet-specific error occurs
+   * @throws IOException
+   *           if an I/O error occurs
+   */
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    processRequest(request, response);
+  }
+
+}
