31d3b384922441112b9beb4ded6f65dde601cb28
Nathan Bunker <nbunker@immregistries.org>
2026-02-19 11:48:58 -0700

    Bumped version.

--- src/main/java/org/openimmunizationsoftware/pt/SoftwareVersion.java
+++ src/main/java/org/openimmunizationsoftware/pt/SoftwareVersion.java
@@ -1,5 +1,5 @@
 package org.openimmunizationsoftware.pt;
 
 public class SoftwareVersion {
-  public static String VERSION = "4.09.0 2026-02-18";
+  public static String VERSION = "4.10.0 2026-02-19";
 }
